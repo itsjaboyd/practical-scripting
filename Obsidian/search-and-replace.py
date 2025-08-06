5ffafac37703ce8d9b67aa34d56e863453e02f1f
Jason Boyd <jasonboyd99@gmail.com>
2025-08-06 11:53:48 -0600

    Created some working functions in for DV queries.

--- Obsidian/search-and-replace.py
+++ Obsidian/search-and-replace.py
@@ -1,8 +1,32 @@
 import pathlib
 import re
 import common
+import platform
 
-ROOT_DIRECTORY = "/Users/jasonboyd/Tracking/"
+
+if platform.system() == "Darwin":
+    BASE_PATH = "/Users/jasonboyd/Tracking/"
+else:  # use WSL's path to user notes on windows WSL
+    BASE_PATH = "/mnt/c/Users/basonjoyd/Tracking/"
+
+
+def repair_old_meeting(contents):
+    query_regex = (
+        r"`{3}dataview\nTABLE summary, created\nFROM \"Meetings\"\nWHERE "
+        r"any\(map\(attendees, \(a\) \=> a \= this.title\)\)\n`{3}"
+    )
+    replacement = (
+        r"```dataview\n"
+        r'TABLE summary as "Summary", transpired as "Transpired"\n'
+        r'FROM "Meetings"\n'
+        r"WHERE any(map(attendees, (a) => a = this.link))\n"
+        r"SORT created DESC\n"
+        r"```"
+    )
+    match_list = find_match_strings(query_regex, contents)
+    if not match_list:
+        return False
+    return re.sub(query_regex, replacement, contents)
 
 
 def find_match_strings(pattern, contents):
@@ -38,11 +62,26 @@ def repair_files_timed_iso_dates(root_path):
     return results
 
 
+def repair_files_with_function(root_path, regex_function):
+    file_list = common.gather_files(root_path)
+    results = [
+        repair_file_with_function(fp, regex_function) for fp in file_list
+    ]
+    return results
+
+
+def repair_file_with_function(file_path, regex_function):
+    if not callable(regex_function):
+        raise ValueError("Supplied function is uncallable!")
+    contents = common.read_file_contents(file_path)
+    contents = regex_function(contents)
+    return common.write_file_contents(file_path, contents)
+
+
 def main():
-    pattern = r''
-    sarah = ROOT_DIRECTORY + "People/sarah-gregory.md"
-    contents = common.read_file_contents(sarah)
-    print(contents)
+    sarah = BASE_PATH + "People/sarah-gregory.md"
+    result = repair_file_with_function(sarah, repair_old_meeting)
+    print(result)
 
 
 if __name__ == "__main__":
