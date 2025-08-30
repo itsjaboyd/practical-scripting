903873dda437dba692235c21143a4a5e0cc6fcf2
Jason Boyd <jasonboyd99@gmail.com>
2025-08-29 20:48:31 -0600

    Added ideas for high-level obsidian scripting file.

--- Obsidian/obsidian.py
+++ Obsidian/obsidian.py
@@ -1,3 +1,22 @@
 # High-level obsidian note manipulation and modification
 
+import datetime
 
+
+TEMPLATES_PATH = ""
+
+
+def create_templated_note(template_name):
+    pass
+
+
+def gather_interval_files(start, end):
+    pass
+
+
+def gather_popular_files(root_path):
+    pass
+
+
+def build_vault_statistics(root_path):
+    pass
